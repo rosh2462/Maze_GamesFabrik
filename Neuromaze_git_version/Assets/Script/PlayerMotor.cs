2503d99749c04209b8e5151124877a3a988b68cc
IbrvCvrtxxn <iibrahim.ismayilovv@gmail.com>
2023-06-13 18:09:39 +0200

    weaponSwitching_system

--- Neuromaze_git_version/Assets/Script/PlayerMotor.cs
+++ Neuromaze_git_version/Assets/Script/PlayerMotor.cs
@@ -74,3 +74,103 @@ bool crouching;
         speed=5;
     }
 }
+
+
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class PlayerMotor : MonoBehaviour
+// {
+//     private CharacterController controller;
+//     private Vector3 playerVelocity;
+//     private bool isGrounded;
+//     private float speed = 5f;
+//     private float gravity = -9.8f;
+//     private float jumpHeight = 3f;
+//     private bool sprinting;
+//     private bool lerpCrouch;
+//     private float crouchTimer;
+//     private bool crouching;
+//     private bool isRunning;
+
+//     private void Start()
+//     {
+//         controller = GetComponent<CharacterController>();
+//     }
+
+//     private void Update()
+//     {
+//         isGrounded = controller.isGrounded;
+
+//         if (lerpCrouch)
+//         {
+//             crouchTimer += Time.deltaTime;
+//             float p = crouchTimer / 1;
+//             p *= p;
+//             if (crouching)
+//                 controller.height = Mathf.Lerp(controller.height, 1, p);
+//             else
+//                 controller.height = Mathf.Lerp(controller.height, 2, p);
+
+//             if (p > 1)
+//             {
+//                 lerpCrouch = false;
+//                 crouchTimer = 0f;
+//             }
+//         }
+
+//         if (Input.GetKeyDown(KeyCode.LeftShift))
+//         {
+//             Sprint();
+//         }
+
+//         ProcessMovement();
+//     }
+
+//     private void ProcessMovement()
+//     {
+//         Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+//         Vector3 moveDirection = Vector3.zero;
+//         moveDirection.x = input.x;
+//         moveDirection.z = input.y;
+
+//         float currentSpeed = speed;
+//         if (isRunning)
+//             currentSpeed *= 1.5f;
+
+//         controller.Move(transform.TransformDirection(moveDirection) * currentSpeed * Time.deltaTime);
+
+//         playerVelocity.y += gravity * Time.deltaTime;
+//         if (isGrounded && playerVelocity.y < 0)
+//             playerVelocity.y = -2f;
+
+//         controller.Move(playerVelocity * Time.deltaTime);
+//     }
+
+//     public void Jump()
+//     {
+//         if (isGrounded)
+//         {
+//             playerVelocity.y = Mathf.Sqrt(jumpHeight * -3f * gravity);
+//         }
+//     }
+
+//     public void Crouch()
+//     {
+//         crouching = !crouching;
+//         crouchTimer = 0;
+//         lerpCrouch = true;
+//     }
+
+//     public void Sprint()
+//     {
+//         sprinting = !sprinting;
+//         isRunning = sprinting;
+
+//         if (sprinting)
+//             speed = 8;
+//         else
+//             speed = 5;
+//     }
+// }
